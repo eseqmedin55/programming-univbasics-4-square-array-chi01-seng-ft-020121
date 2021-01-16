4571cd450a4c0053e125425adb19852466edec5e
esequiel medina <medinaesequiel@gmail.com>
2021-01-16 22:24:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(numbers)
   new_array = []
   i = 0 
   while i < numbers.length
-  new_array >> numbers[i] * numbers[i])
+  new_array << numbers[i] * numbers[i])
   i += 1
   end 
   return new_array
